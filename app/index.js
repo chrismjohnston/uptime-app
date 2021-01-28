a1020f58b40706c1c8bf80d92b026760f7ec1edf
Christopher Johnston <cmjohnston@gmail.com>
2021-01-28 08:44:48 -0600

    get the method from the http request

--- app/index.js
+++ app/index.js
@@ -14,16 +14,19 @@ const server = http.createServer(function (req, res) {
 
   // Get the path
   const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
 
   // Send the response
-  res.end('Hello World\n')
+  res.end('Hello World\n');
 
   // Log the request path
-  console.log('Request received on path: ' + trimmedPath)
-})
+  console.log('Request received on path: ' + trimmedPath + ' with method: ' + method);
+});
 
 // Start the server and  have it listen on port 3000
 server.listen(3000, function () {
   console.log('The server is listening on port 3000')
-})
+});
