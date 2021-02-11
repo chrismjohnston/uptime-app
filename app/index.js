8e60d3b5da203ba0afce149dfcdbb19dfabf70b5
Christopher Johnston <cmjohnston@gmail.com>
2021-02-11 08:28:37 -0600

    added a way to parse payload from header

--- app/index.js
+++ app/index.js
@@ -6,33 +6,46 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string response
 const server = http.createServer(function (req, res) {
-  // Get the url and parse it
-  const parsedUrl = url.parse(req.url, true);
 
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+	// Get the url and parse it
+	const parsedUrl = url.parse(req.url, true);
 
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
+	// Get the path
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // Get the HTTP method
-  const method = req.method.toLowerCase();
+	// Get the query string as an object
+	const queryStringObject = parsedUrl.query;
 
-  // Get the headers as an object
-  const headers = req.headers;
+	// Get the HTTP method
+	const method = req.method.toLowerCase();
 
-  // Send the response
-  res.end('Hello World\n');
+	// Get the headers as an object
+	const headers = req.headers;
 
-  // Log the request path
-  console.log('Request received with these headers', headers);
+	// Get the payload, if any
+	const decoder = new StringDecoder('utf-8');
+	var buffer = '';
+	req.on('data', function (data) {
+		buffer += decoder.write(data);
+	});
+	req.on('end', function () {
+		buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello World\n');
+  
+    // Log the request path
+    console.log('Request received with this payload: ', buffer);
+
+	});
 });
 
 // Start the server and  have it listen on port 3000
 server.listen(3000, function () {
-  console.log('The server is listening on port 3000')
+	console.log('The server is listening on port 3000');
 });
